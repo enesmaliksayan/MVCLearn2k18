823e9c11e296d3923a2f46c32d5e266f3c4ae075
Enes Malik Sayan <enesmaliksayan@gmail.com>
2018-06-04 15:21:28 +0300

    Add project files.

---
+++ MVCLearn2k18/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCLearn2k18.MvcApplication" Language="C#" %>
---
+++ MVCLearn2k18/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCLearn2k18.Startup))]
+namespace MVCLearn2k18
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
