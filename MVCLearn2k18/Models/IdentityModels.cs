2be53b9c763553a9c42d78579d55b525cfa204bd
Enes Malik Sayan <enesmaliksayan@gmail.com>
2018-06-08 15:17:52 +0300

    Migration eklendi, code-first giriş.

--- MVCLearn2k18/Models/IdentityModels.cs
+++ MVCLearn2k18/Models/IdentityModels.cs
@@ -20,6 +20,9 @@ namespace MVCLearn2k18.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+
+        public DbSet<Customer> Customers { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
